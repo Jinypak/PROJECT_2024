8835c8eb127245716b340a174f7448e5d5b7660d
jiny3360 <jiny3360@naver.com>
2024-01-12 13:55:08 +0900

    01121354 : PROJECT INIT - import safenet package

---
+++ Main.java
@@ -0,0 +1,8 @@
+import com.safenetinc.luna.LunaSlotManager;
+
+public class Main {
+    public static void main(String[] args) {
+        LunaSlotManager slotManager = LunaSlotManager.getInstance();
+        System.out.println("SlotManager: " + slotManager);
+    }
+}
