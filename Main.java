dd43b585b51ac0825d20ce6a63738e9feac0143b
jiny3360 <jiny3360@naver.com>
2024-03-07 16:59:24 +0900

    20240307 #1

--- Main.java
+++
@@ -1,8 +0,0 @@
-import com.safenetinc.luna.LunaSlotManager;
-
-public class Main {
-    public static void main(String[] args) {
-        LunaSlotManager slotManager = LunaSlotManager.getInstance();
-        System.out.println("SlotManager: " + slotManager);
-    }
-}
